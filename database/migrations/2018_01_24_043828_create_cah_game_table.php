2a353aeccab09ba8a8566360ab3436198155add3
Ellie Strejlau <ellie.strejlau@gmail.com>
2018-01-23 23:46:40 -0500

    Run artisan migrate after deploy and add migration for CAHGame model.

--- database/migrations/2018_01_24_043828_create_cah_game_table.php
+++ database/migrations/2018_01_24_043828_create_cah_game_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCahGameTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('cah_game', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('thread_id', 17);
+            $table->json('players');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('cah_game');
     }
 }
