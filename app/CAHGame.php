6bc8e978256f60792f3516c4677e16b62800a4eb
Ellie Strejlau <ellie.strejlau@gmail.com>
2018-01-24 01:48:19 -0500

    Test interactive message.

--- app/CAHGame.php
+++ app/CAHGame.php
@@ -20,12 +20,15 @@ class CAHGame extends Model
 
         $this->bot = $bot;
 
+        $num_players_opts = [];
         for($i=4; $i<=10; $i++) {
             $num_players_opts[] = [
                 'text' => $i,
                 'value' => $i
             ];
         }
+
+        Log::debug($num_players_opts);
 
         $bot->replyInThread('How many players?', [
             "attachments" => [
