1644d0fe6d6ebe5a0037d5fe2cdc6279b42d4075
Ellie Strejlau <estrejlau@phase2technology.com>
2018-08-17 16:45:33 -0400

    Try logging?

--- app/Http/Controllers/KarmaController.php
+++ app/Http/Controllers/KarmaController.php
@@ -9,6 +9,8 @@ class KarmaController extends Controller
 {
     public function parse($data) {
       Log::notice(print_r($data, true));
+      Log::notice('HI.');
+      error_log('This is the error log.');
     }
 
     private function add(User $user) {
--- routes/slackbot.php
+++ routes/slackbot.php
@@ -12,17 +12,8 @@ $slackbot->hearsMention('\-\-', function(SlackBot $bot) {
     $bot->addReaction('disapproval');
 });
 
-// TODO: Figure out what method to check for when hearing a mention from another app, because this message comes from @plusplus.
-//$slackbot->hearsMention('now at \d+ points', function(SlackBot $bot) {
-//    $bot->addReaction('fuckyeah');
-//});
-
 $slackbot->hearsMention('(hello|hi)', function(SlackBot $bot) {
     $bot->addReaction('wave');
-});
-
-$slackbot->hearsMention('(screw|fuck) you', function(SlackBot $bot) {
-  $bot->addReaction('disapproval');
 });
 
 $slackbot->hears('^(good morning|morning everyone|guten tag|bom dia|buenos dias|good day|good evening|good night|goodnight)', function(SlackBot $bot) {
