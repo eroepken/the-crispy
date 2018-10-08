d289dc82f14643124f3c7dc6f0bbad05bbb72e31
Ellie Strejlau <estrejlau@phase2technology.com>
2018-10-08 14:37:34 -0400

    Test out queues to handle karma.

--- app/Jobs/ChangeKarmaJob.php
+++ app/Jobs/ChangeKarmaJob.php
@@ -7,6 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
 
 class ChangeKarmaJob implements ShouldQueue
 {
